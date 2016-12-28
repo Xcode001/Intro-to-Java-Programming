209e5cc3ccc18ecbc0e6967831dd36901258dcd4
JStephens33 <jstep33@gmail.com>
2016-12-28 15:47:54 +0000

    Update Exercise_03_26
    Fixed the results. The proposed change displays the input from the user.

--- Exercise_03/Exercise_03_26/Exercise_03_26.java
+++ Exercise_03/Exercise_03_26/Exercise_03_26.java
@@ -16,11 +16,11 @@ public class Exercise_03_26 {
 
 		// Determine whether it is divisible by 5 and 6
 		// Display results
-		System.out.println("Is 10 divisible by 5 and 6? " +
+		System.out.println("Is " + number + " divisible by 5 and 6? " +
 			((number % 5 == 0) && (number % 6 == 0)));
-		System.out.println("Is 10 divisible by 5 or 6? " +
+		System.out.println("Is " + number + " divisible by 5 or 6? " +
 			((number % 5 == 0) || (number % 6 == 0)));
-		System.out.println("Is 10 divisible by 5 of 6, but not both? " +
+		System.out.println("Is " + number + " divisible by 5 of 6, but not both? " +
 			((number % 5 == 0) ^ (number % 6 == 0)));
 	}
 }
