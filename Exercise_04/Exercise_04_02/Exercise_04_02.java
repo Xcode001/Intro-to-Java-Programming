160a8fa32db6aef3f7d5cb43638a7f3782e863c1
JStephens33 <jstep33@gmail.com>
2017-02-07 11:00:12 -0500

    Update Exercise_04_02.java
    Fixed

--- Exercise_04/Exercise_04_02/Exercise_04_02.java
+++ Exercise_04/Exercise_04_02/Exercise_04_02.java
@@ -16,38 +16,32 @@ Use negative to indicate south and east degrees.
 import java.util.Scanner;
 
 public class Exercise_04_02 {
+	
+	private static final double AVERAGE_EARTH_RADIUS = 6371.01;
+	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		// Getting (x1,y1) (x2,y2) from user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter point 1 (latitude and longitude) in degrees: ");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
 
-		final double RADIUS = 6371.01;	// Constant value
+        System.out.print("Enter point 2 (latitude and longitude) in degrees: ");
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        input.close();
 
-		// Prompt the user to enter the latitude and longitude
-		// of two points on the earth in degrees
-		System.out.print("Enter point 1 (latitude and longitude) in degrees: ");
-		String xy1 = input.nextLine();
-		System.out.print("Enter point 2 (latitude and longitude) in degrees: ");
-		String xy2 = input.nextLine();
+        // Solving for the distance between the two points
+        // Formula: d = radius * arccos(sin(x1) * sin(x2) + cos(x1) * cos(x2) * cos(y1 - y2))
 
-		// Extract x and y values from string
-		int k = xy1.indexOf(',');
-		double x1 = Double.parseDouble(xy1.substring(0, k - 1));
-		double y1 = Double.parseDouble(xy1.substring(k + 2));
-		k = xy2.indexOf(',');
-		double x2 = Double.parseDouble(xy2.substring(0, k - 1));
-		double y2 = Double.parseDouble(xy2.substring(k + 2));
+        double radius = AVERAGE_EARTH_RADIUS;
 
-		// Convert degrees to radians
-		x1 = Math.toRadians(x1);
-		y1 = Math.toRadians(y1);
-		x2 = Math.toRadians(x2);
-		y2 = Math.toRadians(y2);
+        double distance = radius *
+                Math.acos( Math.sin(Math.toRadians(x1)) * Math.sin(Math.toRadians(x2)) +
+                           Math.cos(Math.toRadians(x1)) * Math.cos(Math.toRadians(x2)) *
+                           Math.cos(Math.toRadians(y1 - y2)));
 
-		// Calculate its great circle distance
-		double distance = 
-			RADIUS * Math.acos(Math.sin(x1) * Math.sin(x2) + 
-			Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
 
-		// Display result
-		System.out.println("The distance between the two points is " + distance);
+        System.out.printf("The distance between the two points is %f km", distance);
 	}
 }
